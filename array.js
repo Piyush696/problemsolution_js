95821612ee86bb9c71f3d41b2a941d7a110b9a59
Piyush696 <piyushdutta696@gmail.com>
2023-03-22 22:32:32 +0530

    some fill lastIdexof flat methods

--- array.js
+++ array.js
@@ -30,7 +30,7 @@ const fruits = [
   fruits.forEach(element =>{
     for(each in element){
         if(couterFruits[each]){
-            console.log(couterFruits[each])
+            // console.log(couterFruits[each])
             couterFruits[each] = couterFruits[each] + element[each]
         }
         else {
@@ -38,8 +38,36 @@ const fruits = [
         }
     }
   })
-console.log(couterFruits)
+// console.log(couterFruits)
 
 const array1 = [23,42,76,433,232,33];
 const result = Object.keys(array1);
-console.log(result)
+// console.log(result)
+
+
+//flat()
+// const arr1 = [1, 2, 3, 3, 5, 6, [5,9], 5 ]
+const arr1 = [1, 2, 3, 3, 5, 6, [5,9], 5,[2,9], [1,2,3,4] ]
+//  console.log(arr1.flat())
+// console.log(arr1.flat())
+// console.log(arr1.flat(Infinity))
+
+
+//fill
+const arr2 = [1, 2, 3, 3, 5, 6 ]
+//  console.log(arr2.fill(4))
+//  console.log(arr2.fill(4,3)) 
+//  console.log(arr2.fill(4,3, 3)) 
+
+// lastIndexOf
+const animals = ['dodo', 'tiger', 'fox'];
+//  console.log(animals.lastIndexOf('dodo'))
+
+
+
+//some
+
+const arr3 = [1, 2, 3, 3, 5, 6 ];
+
+console.log(arr3.some((e) => e === 6
+))
